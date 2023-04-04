e6a3dc1ee4cb8cbad751a6979000518998d32442
Cristian <ctapia.macaya@gmail.com>
2023-04-03 21:29:23 -0400

    limpieza de código

--- queries.js
+++ queries.js
@@ -8,11 +8,13 @@ const yargs = _yargs(hideBin(process.argv));
 import { Sequelize, DataTypes, Model } from "sequelize";
 import dotenv from "dotenv";
 
+// Datos iniciales de la base de datos
 const sequelize = new Sequelize("postgres", "postgres", "HuC4-rV.PV6qr!6", {
   host: "db.uxukrikzkfreeoehhypg.supabase.co",
   dialect: "postgres",
 });
 
+// Revisar si la conexión se establece correctamente
 try {
     await sequelize.authenticate();
     console.log('Connection has been established successfully.');
@@ -20,18 +22,18 @@ try {
     console.error('Unable to connect to the database:', error);
   }
 
-
-//Clase extiende del modelo 
+// Clase extiende del modelo 
 class Pacientes extends Model {}
 
-//Definir nombre de la tabla y sus campos
+// Nombre de la tabla y sus campos
 Pacientes.init({ 
     nombre: {type: DataTypes.STRING, allowNull: false}, 
     rut: {type: DataTypes.STRING },
     direccion: {type: DataTypes.STRING }},
     { sequelize, modelName: 'Pacientes' });
 
-// `sequelize.define` retorna el modelo 
+// Retorna el modelo
 console.log(Pacientes === sequelize.models.Pacientes);
 
+// Crear tabla si no existe (no hacer nada si no existe)
 await Pacientes.sync();
