b4a4f7bace1e6419543edf188e1fab5e55e1495f
Loreto_menddro <loretoalmendra.rc@gmail.com>
2023-04-04 21:42:37 -0400

    punto 1, 2 y 3 ok

--- queries.js
+++ queries.js
@@ -1,21 +1,102 @@
-import pool from "./lib/db_connection.js";
+// import sequelize from "./lib/db_connection.js"
 import pg from "pg";
 const { Pool } = pg;
 import { argv } from "node:process";
 import _yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 const yargs = _yargs(hideBin(process.argv));
+import { Sequelize, Model, DataTypes } from "sequelize";
 import dotenv from "dotenv";
-//dotenv.config();
+dotenv.config();
 
-yargs.option({
-    d: { demandOption: false, alias: "descripcion" },
-    f: { demandOption: false, alias: "fecha" },
-    m: { demandOption: false, alias: "monto" },
-    c: { demandOption: false, alias: "cuenta" },
+//3.La especialidad tiene un código y una descripción
+
+
+//Crear conexión
+const sequelize = new Sequelize("postgres", "postgres", process.env.SUPABASE_PASS, {
+  host: "db.uxukrikzkfreeoehhypg.supabase.co",
+  dialect: "postgres",
 });
 
-const params = yargs.argv;
 
-pool.connect();
-console.log("holi")
+try {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
+
+
+class Especialidad extends Model { }
+class Medico extends Model { }
+
+Especialidad.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  codigo: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  descripcion: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+
+}, { sequelize, modelName: 'Especialidad' });
+
+// Crear tabla si no existe (no hacer nada si no existe)
+
+Medico.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  nombre: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  rut: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  direccion: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  }
+}, { sequelize, modelName: 'Medico' });
+
+// Crear tabla si no existe (no hacer nada si no existe)
+
+//Enlace tipo model
+Especialidad.hasMany(Medico);
+// Usuario.hasMany(Publicacion, { as: 'publicaciones' });
+Medico.belongsTo(Especialidad);
+
+await sequelize.sync({ force: true });
+
+// Publicacion.belongsTo(Usuario, { as: 'usuario' });
+
+/* Medico.findByPk(1, { include: '' })
+  .then(Medico => {
+    console.log(Medico.Especialidades);
+  }); */
+/* (async () => {
+  await sequelize.sync({ force: false });
+  console.log("Tablas creadas")
+})();
+
+Especialidad.belongsTo(Medico, {
+  foreignKey: "id_medico"
+});
+
+Medico.hasMany(Especialidad, {
+  foreignKey: "id_medico"
+}); */
+
+
